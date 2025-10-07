d51ae220a24c19c5413318a178c6c89bd649c81d
OIMAOUEN <oussamaimaouen@gmail.com>
2025-10-07 12:19:22 +0200

    Update streamlit_app.py
    add search_col

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,9 @@ st.write(
 
 cnx=st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 name_on_order = st.text_input('Name on Smoothie : ')
 st.write(' the name on your moothie will be : ', name_on_order)
